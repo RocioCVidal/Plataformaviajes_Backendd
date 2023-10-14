11ca08a85c457e42be965f4b69170bbd91b79823
RocioCVidal <rcastanedav2@upao.edu.pe>
2023-10-14 01:24:48 -0500

    first commit

---
+++ src/main/java/pe/edu/upao/sistemas/plataformaviajes/PlataformaviajesApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.sistemas.plataformaviajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaviajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaviajesApplication.class, args);
+	}
+
+}
